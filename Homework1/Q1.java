b83e73b2a14b2c176e0ceb5a73ba85e9ddcb4adc
ahnty0122 <ahnty0122@naver.com>
2021-03-25 01:39:05 +0900

    문제 1, 2번

---
+++ Homework1/Q1.java
@@ -0,0 +1,19 @@
+// 두 정수 입력받아 합 구해 출력하는 프로그램.
+import java.util.Scanner;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner;
+		System.out.print("두 정수를 입력하세요>>");
+		
+		scanner = new Scanner(System.in); 
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.print(a + "+" + b + "은 " + (a + b));
+	}
+
+}
---
+++ Homework1/Q2.java
@@ -0,0 +1,17 @@
+// 한 층 높이 5m일 때, 건물이 몇 층인지 입력받아 높이를 출력.
+import java.util.Scanner;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner;
+		System.out.println("몇 층인지 입력하세요>>");
+		
+		scanner = new Scanner(System.in);
+		int floor = scanner.nextInt();
+		
+		System.out.println(floor*5 + "m " + "입니다.");		
+	}
+
+}
