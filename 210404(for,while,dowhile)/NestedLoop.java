12a6f541668dfe9ff08e54a7e9f3f497d6b8f963
ahnty0122 <ahnty0122@naver.com>
2021-04-04 17:34:54 +0900

    210404 반복문

---
+++ 210404(for,while,dowhile)/NestedLoop.java
@@ -0,0 +1,15 @@
+
+public class NestedLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for(int i = 0; i < 10; i++) {
+				for(int j = 0; j < 10; j++) {
+					System.out.print(i + "*" + j + "=" +  i * j);
+					System.out.print('\t');					
+				}
+				System.out.println();
+			}
+	}
+
+}
