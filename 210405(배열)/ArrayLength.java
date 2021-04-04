2939839f9ade1d906cc37bdbe9eab1d2b34c304d
ahnty0122 <ahnty0122@naver.com>
2021-04-05 00:56:02 +0900

    배열

---
+++ 210405(배열)/ArrayLength.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ArrayLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("5개의 정수를 입력하세요");
+			int intArray[] = new int[5];
+			double sum = 0.0;
+			
+			for(int i = 0; i < intArray.length; i++) {
+				intArray[i] = scanner.nextInt();
+				sum += intArray[i];	
+			}
+			System.out.print("평균은" + sum / intArray.length);
+			
+			scanner.close();
+	}
+
+}
