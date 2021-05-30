14ce848bb3330a2415872a8d5d1147a516386160
ahnty0122 <ahnty0122@naver.com>
2021-05-30 16:39:02 +0900

    독립 클래스 수정

--- quiz/quiz1_2.java
+++ quiz/quiz1_2.java
@@ -4,21 +4,20 @@ import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class quiz1_2 extends JFrame {
 	
 	JLabel la = new JLabel("r=red y=yellow b=blue");
-	Container c = getContentPane();
 	
 	public quiz1_2() {
 		super("키보드 이벤트 예제 - 독립 클래스");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
 		c.add(la);
-		c.addKeyListener(new MyKeyListener());
+		c.addKeyListener(new MyKeyListener(this));
 		
 		c.setFocusable(true);
 		c.requestFocus();
@@ -33,24 +32,23 @@ public class quiz1_2 extends JFrame {
 }
 
 class MyKeyListener extends KeyAdapter {
+	quiz1_2 superClass;
 	@Override
 	public void keyPressed(KeyEvent e) {
 		
 		Component c = (Component)e.getSource();
 		
-		JLabel la = (JLabel)e.getComponent();
-		
 		switch (e.getKeyChar()) {
 		case 'r':
-			la.setText("붉은색입니다");			
+			superClass.la.setText("붉은색입니다");			
 			c.setBackground(Color.RED);
 			break;
 		case 'b':
-			la.setText("파란색입니다");				
+			superClass.la.setText("파란색입니다");				
 			c.setBackground(Color.BLUE);
 			break;
 		case 'y':
-			la.setText("노란색입니다");		
+			superClass.la.setText("노란색입니다");		
 			c.setBackground(Color.YELLOW);
 			break;
 		case 'q':
@@ -60,5 +58,10 @@ class MyKeyListener extends KeyAdapter {
 		}
 		
 		super.keyPressed(e);
+	}
+	
+	MyKeyListener(quiz1_2 superClass)
+	{
+		this.superClass = superClass;
 	}
 }
